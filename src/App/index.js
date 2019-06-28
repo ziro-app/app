4fc9959ae38d26e6b55162a21bf462383171ce95
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-28 09:26:48 -0300

    test code to access db

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { connectDb } from './utils/connectDb'
 
 export const App = () => {
-	useEffect(() => connectDb())
+	useEffect(() => {
+		connectDb()
+	}, [])
 	return <div>App</div>
 }
--- src/App/utils/connectDb.js
+++ src/App/utils/connectDb.js
@@ -1,24 +1,19 @@
 import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+/* variables */
 const APP_ID = process.env.APP_ID
 const SERVICE_NAME = process.env.SERVICE_NAME
 const DB_NAME = process.env.DB_NAME
+const factory = RemoteMongoClient.factory
+const credential = new AnonymousCredential()
 
 export const connectDb = async () => {
 	try {
 		const client = Stitch.initializeDefaultAppClient(APP_ID)
-		const db = client.getServiceClient(RemoteMongoClient.factory, SERVICE_NAME).db(DB_NAME)
-		const result = await client.auth.loginWithCredential(new AnonymousCredential())
-		console.log(result)
-		// client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
-		//   db.collection('Users').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
-		// ).then(() =>
-		//   db.collection('Users').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
-		// ).then(docs => {
-		//     console.log("Found docs", docs)
-		//     console.log("[MongoDB Stitch] Connected to Stitch")
-		// }).catch(err => {
-		//     console.error(err)
-		// })
+		const db = client.getServiceClient(factory, SERVICE_NAME).db(DB_NAME)
+		const { id: userId } = await client.auth.loginWithCredential(credential)
+		console.log(userId)
+		const users = await db.collection('Users').find({ owner_id: userId }).asArray()
+		console.log(users)
 	} catch (error) {
 		console.log(error)
 	}
