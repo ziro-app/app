01b3e130637ae4daeb2982caaa88b265e654dc1d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-10 15:08:08 -0300

    add logo component

---
+++ src/App/Logo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Image } from 'cloudinary-react'
+
+export const Logo = () =>
+    <Image
+    	cloudName='ziro'
+    	width='45'
+    	publicId='logo-app_fwothv'
+    	version='1561160634'
+    	format='png'
+    	secure='true'
+    	alt='logo'
+    />
