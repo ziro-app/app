36381f4964e24dbccbeb3161423bc4f835b47d6e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-12 09:28:02 -0300

    add error messages

--- src/App/Login/index.js
+++ src/App/Login/index.js
@@ -1,13 +1,20 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { validateForm } from './utils/validateForm'
 import { container, inputWrapper, input, forgotPass, submit } from './styles'
 
 export const Login = () => {
 	const inputEmail = useRef(null)
+	const [errorEmail, setErrorEmail] = useState('')
 	const inputPass = useRef(null)
+	const [errorPass, setErrorPass] = useState('')
 	const handleSubmit = event => {
 		event.preventDefault()
-		const formIsValid = validateForm(inputEmail.current.value, inputPass.current.value)
+		const { errorMsgEmail, errorMsgPassword, formIsValid } = validateForm(
+			inputEmail.current.value,
+			inputPass.current.value
+		)
+		setErrorEmail(errorMsgEmail)
+		setErrorPass(errorMsgPassword)
 		console.log(inputEmail.current.value)
 		console.log(inputPass.current.value)
 		if (formIsValid)
@@ -20,10 +27,12 @@ export const Login = () => {
 			<div style={inputWrapper}>
 				<label htmlFor='email'>Email</label>
 				<input ref={inputEmail} style={input} type='text' name='email' id='email' />
+				{errorEmail}
 			</div>
 			<div style={inputWrapper}>
 				<label htmlFor='password'>Senha</label>
 				<input ref={inputPass} style={input} type='password' name='password' id='password' />
+				{errorPass}
 			</div>
 			<input style={submit} type='submit' value='Acessar' />
 			<label style={forgotPass}>Esqueci a senha</label>
--- src/App/Login/utils/validateForm.js
+++ src/App/Login/utils/validateForm.js
@@ -1,5 +1,12 @@
 export const validateForm = (email, password) => {
-	const emailIsValid = !!email
-	const passwordIsValid = !!password
-	return emailIsValid && passwordIsValid
+	const emailIsValid = /^\S+@\S+\.\S+$/g.test(email) // tests for pattern a@b.c
+	const passwordIsValid = !/^.{0,7}$/g.test(password) // tests for min length of 8 char
+	const errorMsgEmail = emailIsValid ? '' : 'Formato inválido'
+	const errorMsgPassword = passwordIsValid ? '' : 'Mínimo de 8 caracteres'
+	const formIsValid = emailIsValid && passwordIsValid
+	return {
+		formIsValid,
+		errorMsgEmail,
+		errorMsgPassword
+	}
 }
