1c8354acd848c2ae8e89e67482a0aa526f794f9e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-17 12:06:59 -0300

    style menu tabs on click to show navigation

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { A } from 'hookrouter'
+import { usePath, A } from 'hookrouter'
 import { Logo } from '../../Assets/Logo/index'
 import { container, menu, tab, tabActive } from './styles'
 
 export const Header = () => {
+	const path = usePath()
 	return (
 		<div style={container}>
 			<Logo />
 			<div style={menu}>
-				<A style={tabActive} href='/login'>Login</A>
-				<A style={tab} href='/cadastrar'>Cadastrar</A>
+				<A style={path !== '/cadastrar' ? tabActive : tab} href='/login'>Login</A>
+				<A style={path === '/cadastrar' ? tabActive : tab} href='/cadastrar'>Cadastrar</A>
 			</div>
 		</div>
 	)
