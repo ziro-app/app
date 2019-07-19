0e1d7f4b7fde3d8fe66072ee41b418d70ac98500
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-19 16:10:52 -0300

    start migrating from hookrouter to wouter

--- src/App/Router/index.js
+++ src/App/Router/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useRoutes } from 'hookrouter'
+import { useLocation } from 'wouter'
 import { Header } from '../Header/index'
 import { Login } from '../Login/index'
 import { Register } from '../Register/index'
@@ -8,25 +8,40 @@ import { MyData } from '../MyData/index'
 
 
 const privateRoutes = {
-	'/': () => user => <MyData user={user} />,
-	'/meus-dados': () => user => <MyData user={user} />
+	'/': user => <MyData user={user} />,
+	'/meus-dados': user => <MyData user={user} />
 }
 
 const publicRoutes = {
-	'/login': () => <div><Header /><Login /></div>,
-	'/cadastrar': () => <div><Header /><Register /></div>,
-	'/*': () => <div><Header /><Login /></div>
+	'/login': <div><Header /><Login /></div>,
+	'/cadastrar': <div><Header /><Register /></div>,
+	'/': <div><Header /><Login /></div>
 }
 
-export const Router = ({ user, setUser }) => {
-	if (user) {
-		const routeFound = useRoutes(privateRoutes)
-		if (!routeFound)
-			return <div>Not found</div>
-		return routeFound(user)
-	}
-	return useRoutes(publicRoutes)
+export const Router = ({ user, setUser, location }) => {
+	// console.log('user',user)
+	// console.log('location',location)
+	// console.log('public',publicRoutes[location])
+	// console.log('private',privateRoutes[location])
+	if (user)
+		if (privateRoutes[location])
+			return privateRoutes[location](user)
+		else return <div>Not found</div>
+	else
+		if (publicRoutes[location])
+			return publicRoutes[location]
+		else return <div>Not found</div>
 }
+
+// export const Router = ({ user, setUser }) => {
+// 	if (user) {
+// 		const routeFound = useRoutes(privateRoutes)
+// 		if (!routeFound)
+// 			return <div>Not found</div>
+// 		return routeFound(user)
+// 	}
+// 	return useRoutes(publicRoutes)
+// }
 
 Router.propTypes = {
 	user: PropTypes.object,
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../Firebase/db'
-import { usePath, navigate } from 'hookrouter'
+import { useLocation } from 'wouter'
 import ErrorBoundary from './ErrorBoundary/index'
 import { InitialLoader } from './InitialLoader/index'
 import { Router } from './Router/index'
@@ -8,20 +8,21 @@ import { Router } from './Router/index'
 export const App = () => {
 	const [user, setUser] = useState(null)
 	const [isLoading, setIsLoading] = useState(true)
-	const path = usePath()
+	const [location, setLocation] = useLocation()
 	useEffect(() => db.auth().onAuthStateChanged(user => {
 		if (user) {
 			setUser(user)
-			if (/(^\/cadastrar$)|(^\/login$)/g.test(path)) 
-				navigate('/')
-			else navigate(path)
+			console.log(location)
+			if (/(^\/cadastrar$)|(^\/login$)/g.test(location)) 
+				setLocation('/')
+			console.log(location)
 		}
 		else setUser(null)
 		if (setIsLoading) setIsLoading(false)
 	}), [])
 	return (
 		<ErrorBoundary>
-			{isLoading ? <InitialLoader /> : <Router user={user} setUser={setUser} />}
+			{isLoading ? <InitialLoader /> : <Router user={user} setUser={setUser} location={location} />}
 		</ErrorBoundary>
 	)
 }
