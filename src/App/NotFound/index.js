a61a26f38f82f2100cba3f9dbb1363a18cd9be41
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-21 09:59:45 -0300

    finish styling NotFound component

--- src/App/NotFound/index.js
+++ src/App/NotFound/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'wouter'
 import { Logo } from '../../Assets/Logo/index'
-import { container, title } from './styles'
+import { container, logo, title, body, button } from './styles'
 
 export const NotFound = () =>
 	<div style={container}>
-	    <Logo />
+	    <div style={logo}><Logo /></div>
 		<h1 style={title}>Página não existe</h1>
-		<p>Retorne à pagina inicial para continuar navegando</p>
-		<p>Retornar</p>
+		<p style={body}>Retorne à pagina inicial para continuar navegando</p>
+		<Link href='/'><input style={button} type='submit' value='Retornar' /></Link>
 	</div>
--- src/App/NotFound/styles.js
+++ src/App/NotFound/styles.js
@@ -1,11 +1,18 @@
-import { maxWidth, standardMargin, standardPadding, primaryColor, alertColor, fontTitle, fontWeightTitle } from '../../Theme/styleVariables'
+import { maxWidth, standardMargin, standardPadding,
+	primaryColor, fontTitle, fontWeightTitle, shadow } from '../../Theme/styleVariables'
+
 export const 
 
 container = {
+	display: 'grid',
 	maxWidth: maxWidth,
 	margin: standardMargin,
 	padding: standardPadding,
 	textAlign: 'center'
+},
+
+logo = {
+	justifySelf: 'center'
 },
 
 title = {
@@ -13,4 +20,23 @@ title = {
 	fontFamily: fontTitle,
 	fontWeight: fontWeightTitle,
 	textTransform: 'uppercase'
+},
+
+body = {
+	margin: '12px 0'
+},
+
+button = {
+	marginTop: '12px',
+	WebkitAppearance: 'none',
+	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+	MozAppearance: 'none',
+	outline: 'none',
+	cursor: 'pointer',
+	padding: '10px 0px',
+	border: `1px solid ${primaryColor}`,
+	borderRadius: '20px',
+	color: '#FFF',
+	backgroundColor: primaryColor,
+	boxShadow: `${shadow}`
 }
