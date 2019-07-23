c14eb39d58fd60570124048132dcf0bf47da24ce
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-23 17:19:23 -0300

    refactor public router component

--- src/App/Router/PublicRouter.js
+++ src/App/Router/PublicRouter.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Switch, Route } from 'wouter'
+import { Switch, Route, Redirect } from 'wouter'
 import { Header } from '../Header/index'
 import { SlideRoute } from '../SlideRoute/index'
 import { Login } from '../Login/index'
 import { Register } from '../Register/index'
 
-export const PublicRouter = () =>
-		<Route path='/:any*'>
-			<Header primaryTab='/login' secondaryTab='/cadastrar'>
-				<Switch>
-					<Route path='/login'>
-						<SlideRoute fromPath='/login' toPath='/cadastrar'>
-							<Login />
-						</SlideRoute>
-					</Route>
-					<Route path='/cadastrar'>
-						<SlideRoute fromPath='/cadastrar' toPath='/login'>
-							<Register />
-						</SlideRoute>
-					</Route>
-				</Switch>
-			</Header>
-		</Route>
+export const PublicRouter = () =>	
+	<Header primaryTab='/login' secondaryTab='/cadastrar'>
+		<Switch>
+			<Route path='/login'>
+				<SlideRoute fromPath='/login' toPath='/cadastrar'>
+					<Login />
+				</SlideRoute>
+			</Route>
+			<Route path='/cadastrar'>
+				<SlideRoute fromPath='/cadastrar' toPath='/login'>
+					<Register />
+				</SlideRoute>
+			</Route>
+			<Route path='/:any*'>
+				<Redirect to='/login' />
+			</Route>
+		</Switch>
+	</Header>
