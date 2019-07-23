c772b8e4fbd704648d43708b6c329d3561596fef
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-23 17:20:38 -0300

    minor refactor of main index.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../Firebase/db'
-import { useLocation } from 'wouter'
 import ErrorBoundary from './ErrorBoundary/index'
 import { InitialLoader } from './InitialLoader/index'
 import { Router } from './Router/index'
@@ -8,7 +7,6 @@ import { Router } from './Router/index'
 export const App = () => {
 	const [user, setUser] = useState(null)
 	const [isLoading, setIsLoading] = useState(true)
-	const [location, setLocation] = useLocation()
 	useEffect(() => db.auth().onAuthStateChanged(user => {
 		if (user) setUser(user)
 		else setUser(null)
@@ -16,7 +14,7 @@ export const App = () => {
 	}), [])
 	return (
 		<ErrorBoundary>
-			{isLoading ? <InitialLoader /> : <Router user={user} setUser={setUser} location={location} setLocation={setLocation} />}
+			{isLoading ? <InitialLoader /> : <Router user={user} />}
 		</ErrorBoundary>
 	)
 }
