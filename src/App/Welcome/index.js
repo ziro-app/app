741b7d7e0a11011e619cb1900b643df31a563d52
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-29 08:43:21 -0300

    finish styling welcome screen

--- src/App/Welcome/index.js
+++ src/App/Welcome/index.js
@@ -3,26 +3,27 @@ import { Link } from 'wouter'
 import { ShoppingIcon } from '../../Assets/ShoppingIcon/index'
 import { TruckIcon } from '../../Assets/TruckIcon/index'
 import { CardIcon } from '../../Assets/CardIcon/index'
-import { container, welcome, subtitle, dot, wrapper, label, instructions, icon, text, cnae, submit } from './styles'
+import { container, welcome, subtitle, dot, benefits, wrapper, label, icon, text, submit } from './styles'
 
 export const Welcome = () =>
 	<div style={container}>
 		<h1 style={welcome}><label style={subtitle}>Faça parte</label><label style={dot}>.</label><br/>Crie uma conta</h1>
-		<div style={instructions}>
+		<div style={benefits}>
+			<p>Compre no atacado do Bom Retiro, Brás e Itaim para revender</p>
 			<div style={wrapper}>
-				<label style={label} htmlFor='cnpj'><div style={icon}><ShoppingIcon size={15} /></div><label style={cnae}>Assessoria</label></label>
-				<p style={text}>Encontre as melhores peças para seu público-alvo</p>
+				<label style={label}><div style={icon}><ShoppingIcon size={15} /></div>Assessoria</label>
+				<p style={text}>Encontre as melhores peças para o seu público-alvo</p>
 			</div>
 			<div style={wrapper}>
-				<label style={label} htmlFor='cnpj'><div style={icon}><TruckIcon size={15} /></div><label style={cnae}>Logística</label></label>
-				<p style={text}>Despreocupe-se com retirada e despacho de mercadorias</p>
+				<label style={label}><div style={icon}><TruckIcon size={15} /></div>Logística</label>
+				<p style={text}>Despreocupe-se com a retirada e o despacho de mercadorias</p>
 			</div>
 			<div style={wrapper}>
-				<label style={label} htmlFor='cnpj'><div style={icon}><CardIcon size={15} /></div><label style={cnae}>Pagamento</label></label>
-				<p style={text}>Parcele em até 6x via link seguro ou maquininha</p>
+				<label style={label}><div style={icon}><CardIcon size={15} /></div>Pagamento</label>
+				<p style={text}>Parcele em até 6x no cartão via link seguro ou maquininha</p>
 			</div>
 		</div>
-		<Link href=''>
+		<Link href='/cadastrar/cnpj'>
 			<input style={submit} type='submit' value='Iniciar' />
 		</Link>
 	</div>
--- src/App/Welcome/styles.js
+++ src/App/Welcome/styles.js
@@ -26,10 +26,24 @@ dot = {
 	fontSize: '2.1rem'
 },
 
-instructions = {
+benefits = {
 	display: 'grid',
-	gridRowGap: '15px',
+	gridRowGap: '30px'
+},
+
+wrapper = {
+	display: 'grid',
+	gridRowGap: '5px',
 	color: primaryColor
+},
+
+label = {
+	display: 'grid',
+	gridTemplateColumns: 'auto 1fr',
+	gridColumnGap: '8px',
+	alignItems: 'center',
+	fontFamily: fontTitle,
+	fontSize: '1.7rem'
 },
 
 icon = {
@@ -38,78 +52,15 @@ icon = {
 	width: '30px',
 	height: '30px',
 	borderRadius: '100%',
-	boxShadow: `0px 3px 10px -5px rgba(34,34,34,0.65)`
+	boxShadow: `0px 3px 11px -4px rgba(34,34,34,0.7)`
 },
 
 text = {
 	fontSize: '1.6rem'
 },
 
-cnae = {
-	fontFamily: fontTitle,
-	fontSize: '1.5rem'
-},
-
-form = {
-	display: 'grid',
-	gridRowGap: '20px'
-},
-
-wrapper = {
-	display: 'grid',
-	gridRowGap: '8px',
-	color: primaryColor
-},
-
-label = {
-	display: 'grid',
-	gridTemplateColumns: 'auto 1fr auto',
-	gridColumnGap: '10px',
-	alignItems: 'center',
-	marginBottom: '-4px'
-},
-
-input = {
-	WebkitAppearance: 'none',
-	MozAppearance: 'none',
-	outline: 'none',
-	padding: '8px 24px',
-	border: `2px solid ${grayColor3}`,
-	borderRadius: '8px',
-	color: primaryColor,
-	backgroundColor: '#FDFDFD',
-	boxShadow: `rgba(34,34,34,0.3) 0px 3px 10px -3px`
-},
-
-inputCnpj = {
-	...input,
-	textAlign: 'center'
-},
-
-error = {
-	display: 'flex',
-	alignItems: 'center',
-	fontSize: fontSizeSmall,
-	color: alertColor,
-	fontWeight: fontWeightBody
-},
-
-buttonWrapper = {
-	display: 'grid',
-	gridRowGap: '20px',
-	textAlign: 'center',
-	marginTop: '-10px'
-},
-
-loader = {
-	display: 'grid',
-	alignItems: 'center',
-	justifyItems: 'center',
-	height: '30px',
-	marginBottom: '-10px'
-},
-
 submit = {
+	marginTop: '20px',
 	WebkitAppearance: 'none',
 	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
 	MozAppearance: 'none',
@@ -124,20 +75,6 @@ submit = {
 	boxShadow: `${shadow}`
 },
 
-submitDisabled = {
-	...submit,
-	cursor: 'initial',
-	color: primaryColor,
-	backgroundColor: '#FFF',
-	boxShadow: 'none'
-},
-
 scaleButton = {
 	scale: 0.95
-},
-
-
-forgotPass = {
-	fontSize: fontSizeSmall,
-	color: primaryColor
 }
