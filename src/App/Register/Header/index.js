22fc8c3f3a526c7839dc974bcc18ec74e5c541ba
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-07-29 15:57:32 -0300

    add close icon

--- src/App/Register/Header/index.js
+++ src/App/Register/Header/index.js
@@ -1,17 +1,25 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { useLocation } from 'wouter'
+import { CloseIcon } from '../../../Assets/CloseIcon/index'
 import { BackIcon } from '../../../Assets/BackIcon/index'
 import { Logo } from '../../../Assets/Logo/index'
 import { container, back } from './styles'
 
-export const Header = ({ children }) =>
-	<Fragment>
-		<div style={container}>
-			<div style={back}><BackIcon /></div>
-			<Logo />
-		</div>
-		{children}
-	</Fragment>
+export const Header = ({ children }) => {
+	const [location] = useLocation()
+	return (
+		<Fragment>
+			<div style={container}>
+				<div style={back}>
+					{location === '/cadastrar/cnpj' ? <CloseIcon /> : <BackIcon />}
+				</div>
+				<Logo />
+			</div>
+			{children}
+		</Fragment>
+	)
+}
 
 Header.propTypes = {
 	children: PropTypes.element.isRequired
