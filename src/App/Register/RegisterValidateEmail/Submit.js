f8ba166aa03d79f6a1ec3b6050f9953b46dd0380
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-08-06 14:08:31 -0300

    add variable to track submitting state

--- src/App/Register/RegisterValidateEmail/Submit.js
+++ src/App/Register/RegisterValidateEmail/Submit.js
@@ -1,28 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PropTypes } from 'prop-types'
 import { useLocation } from 'wouter'
 import { motion } from 'framer-motion'
 import { db } from '../../../Firebase/db'
-import { primary, secondary, scaleButton } from './styles'
+import { Errors } from './Errors'
+import { Spinner } from '../../../Assets/Spinner/index'
+import { buttonWrapper, loader, primary, secondary, scaleButton } from './styles'
 
 export const Submit = ({ callToAction, type }) => {
+	const [submitting, setSubmitting] = useState(false)
 	const [, setLocation] = useLocation()
 	const handleClick = async type => {
 		if (type === 'primary') {
 			setLocation('/home/login')
 			window.location.reload()
 		}
-		else
+		else {
+			setSubmitting(true)
 			await db.auth().currentUser.sendEmailVerification({ url: process.env.CONTINUE_URL })
+			setSubmitting(false)
+		}
 	}
 	return (
-		<motion.input
-			style={type === 'primary' ? primary : secondary }
-			type='submit'
-			value={callToAction}
-			whileTap={scaleButton}
-			onClick={handleClick.bind(null, type)}
-		/>
+		<div style={buttonWrapper}>
+			<div style={loader}>
+				{submitting ? <Spinner size={'4rem'} /> : <Errors message={errorEmail || errorSubmit} />}
+			</div>
+			<motion.input
+				style={type === 'primary' ? primary : secondary }
+				type='submit'
+				value={callToAction}
+				whileTap={submitting ? null : scaleButton}
+				onClick={handleClick.bind(null, type)}
+			/>
+		</div>
 	)
 }
 
