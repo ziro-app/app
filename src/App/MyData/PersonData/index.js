d4918d79b733d86de7d9d40218527ad280a62313
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-08-23 17:54:10 -0300

    start inputs disabled but allow for enable on pen icon click

--- src/App/MyData/PersonData/index.js
+++ src/App/MyData/PersonData/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PenIcon } from '../../../Assets/PenIcon/index'
 import { PendingIcon } from '../../../Assets/PendingIcon/index'
 import { SuccessIcon } from '../../../Assets/SuccessIcon/index'
@@ -6,56 +6,60 @@ import { successColor } from '../../../Theme/styleVariables'
 import { container, warning, field, header, headerAlt, label, input, labelWrapper, pending, validated } from './styles'
 
 export const PersonData = () => {
+	const [disableFname, setDisableFname] = useState(true)
+	const [disableLname, setDisableLname] = useState(true)
+	const [disableRG, setDisableRG] = useState(true)
+	const [disableCPF, setDisableCPF] = useState(true)
 	return (
 		<div style={container}>
 			<label style={warning}><PendingIcon size={13} color={'#E5CD00'} strokeWidth={3} />corrija pendências para liberar pagamentos</label>
 			<div style={field}>
-				<div style={header}>
+				<div style={header} onClick={() => setDisableFname(false)}>
 					<label style={label}>Nome</label>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} value='Vitor' />
+				<input style={input} disabled={disableFname} value='Vitor' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={header}>
+				<div style={header} onClick={() => setDisableLname(false)}>
 					<label style={label}>Sobrenome</label>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} value='Barbosa' />
+				<input style={input} disabled={disableLname} value='Barbosa' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={headerAlt}>
+				<div style={headerAlt} onClick={() => setDisableRG(false)}>
 					<label style={label}>RG</label>
 					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>pendente</label></div>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} placeholder='11.22.33.44-55' />
+				<input style={input} disabled={disableRG} placeholder='11.22.33.44-55' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={headerAlt}>
+				<div style={headerAlt} onClick={() => setDisableCPF(false)}>
 					<label style={label}>CPF</label>
 					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>pendente</label></div>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} placeholder='111.222.333-44' />
+				<input style={input} disabled={disableCPF} placeholder='111.222.333-44' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<div style={labelWrapper}><label style={label}>Email</label><label style={validated}><SuccessIcon size={9} color={successColor} />validado</label></div>
-				<input style={input} value='vitorbarbosa19@gmail.com' />
+				<input style={input} disabled={true} value='vitorbarbosa19@gmail.com' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<label style={label}>Whatsapp</label>
-				<input style={input} value='+55 (11) 95177-1321' />
+				<input style={input} disabled={true} value='+55 (11) 95177-1321' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<label style={label}>Nova Senha</label>
-				<input style={input} value='' />
+				<input style={input} disabled={true} value='' />
 				<label>&nbsp;</label>
 			</div>
 		</div>
