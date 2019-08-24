4abb752ab915c8c99f1d0a9f0d1aabeea434412d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-08-24 18:47:10 -0300

    refactor inputs

--- src/App/MyData/PersonData/index.js
+++ src/App/MyData/PersonData/index.js
@@ -8,70 +8,62 @@ import { container, warning, field, header, headerAlt, label, input, labelWrappe
 
 export const PersonData = () => {
 	const [fname, setFname] = useState('Vitor')
-	const [disableFname, setDisableFname] = useState(true)
 	const inputFname = useRef(null)
 	const editFname = () => {
-		setDisableFname(false)
 		if (inputFname && inputFname.current)
 			inputFname.current.select()
 	}
-	const updateFname = ({ target: { value } }) => {
-		setFname(value)
-	}
-	/* --- */
-	const [disableLname, setDisableLname] = useState(true)
-	const [disableRG, setDisableRG] = useState(true)
-	const [disableCPF, setDisableCPF] = useState(true)
+	const updateFname = ({ target: { value } }) => setFname(value)
 	return (
 		<div style={container}>
 			<label style={warning}><PendingIcon size={13} color={'#E5CD00'} strokeWidth={3} />corrija pendências para liberar pagamentos</label>
-			<div style={field}>
-				<div style={header} onClick={editFname}>
+			<div style={field} onClick={editFname}>
+				<div style={header}>
 					<label style={label}>Nome</label>
 					<PenIcon size={13} />
 				</div>
-				<InputText style={input} ref={inputFname} disabled={disableFname} value={fname} onChange={updateFname} />
+				<InputText style={input} ref={inputFname} value={fname} onChange={updateFname} />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={header} onClick={() => setDisableLname(false)}>
+				<div style={header}>
 					<label style={label}>Sobrenome</label>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} readonly={disableLname} value='Barbosa' />
+				<input style={input} value='Barbosa' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={headerAlt} onClick={() => setDisableRG(false)}>
+				<div style={headerAlt}>
 					<label style={label}>RG</label>
 					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>pendente</label></div>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} readonly={disableRG} placeholder='11.22.33.44-55' />
+				<input style={input} placeholder='11.22.33.44-55' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
-				<div style={headerAlt} onClick={() => setDisableCPF(false)}>
+				<div style={headerAlt}>
 					<label style={label}>CPF</label>
 					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>pendente</label></div>
 					<PenIcon size={13} />
 				</div>
-				<input style={input} readonly={disableCPF} placeholder='111.222.333-44' />
+				<input style={input} placeholder='111.222.333-44' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<div style={labelWrapper}><label style={label}>Email</label><label style={validated}><SuccessIcon size={9} color={successColor} />validado</label></div>
-				<input style={input} readonly={true} value='vitorbarbosa19@gmail.com' />
+				<input style={input} disabled={true} value='vitorbarbosa19@gmail.com' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<label style={label}>Whatsapp</label>
-				<input style={input} readonly={true} value='+55 (11) 95177-1321' />
+				<input style={input} disabled={true} value='+55 (11) 95177-1321' />
 				<label>&nbsp;</label>
 			</div>
 			<div style={field}>
 				<label style={label}>Nova Senha</label>
-				<input style={input} readonly={true} value='' />
+				<input style={input} disabled={true} value='' />
 				<label>&nbsp;</label>
 			</div>
 		</div>
