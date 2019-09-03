5f04b34e7c7ff4c52bae418bc0bda44e5ed8d2fa
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-03 17:37:27 -0300

    add more standard data blocks

--- src/App/MyData/PersonData/index.js
+++ src/App/MyData/PersonData/index.js
@@ -1,13 +1,6 @@
-import React, { useState, useRef, useReducer, Fragment } from 'react'
+import React, { useState } from 'react'
 import EditableData from '@bit/vitorbarbosa19.ziro.editable-data'
-import InputText from '@bit/vitorbarbosa19.ziro.input-text'
-import { PenIcon } from '../../../Assets/PenIcon/index'
-import { PendingIcon } from '../../../Assets/PendingIcon/index'
-import { AlertIcon } from '../../../Assets/AlertIcon/index'
-import { SuccessIcon } from '../../../Assets/SuccessIcon/index'
-import { Spinner } from '../../../Assets/Spinner/index'
-import { successColor } from '../../../Theme/styleVariables'
-import { container, block, space, header, name, note, input, inputCss, error, pending, validated, warning, field, headerAlt, spinner, label, labelWrapper, submit } from './styles'
+import { container } from './styles'
 
 export const PersonData = () => {
 	const [fname, setFname] = useState('Vitor')
@@ -34,47 +27,58 @@ export const PersonData = () => {
 				setError={setErrorFname}
 				error={errorFname}
 			/>
-			<div style={field}>
-				<div style={headerAlt}>
-					<label style={label}>Sobrenome</label>
-					<PenIcon size={13} />
-				</div>
-				<input style={input} value='Barbosa' />
-				<label>&nbsp;</label>
-			</div>
-			<div style={field}>
-				<div style={headerAlt}>
-					<label style={label}>RG</label>
-					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>preencha p/ liberar pagamentos</label></div>
-					<PenIcon size={13} />
-				</div>
-				<input style={input} placeholder='11.22.33.44-55' />
-				<label>&nbsp;</label>
-			</div>
-			<div style={field}>
-				<div style={headerAlt}>
-					<label style={label}>CPF</label>
-					<div style={labelWrapper}><PendingIcon size={9} color={'#E5CD00'} strokeWidth={3} /><label style={pending}>preencha p/ liberar pagamentos</label></div>
-					<PenIcon size={13} />
-				</div>
-				<input style={input} placeholder='111.222.333-44' />
-				<label>&nbsp;</label>
-			</div>
-			<div style={field}>
-				<div style={labelWrapper}><label style={label}>Email</label><label style={validated}><SuccessIcon size={9} color={successColor} />validado</label></div>
-				<input style={input} disabled={true} value='vitorbarbosa19@gmail.com' />
-				<label>&nbsp;</label>
-			</div>
-			<div style={field}>
-				<label style={label}>Whatsapp</label>
-				<input style={input} disabled={true} value='+55 (11) 95177-1321' />
-				<label>&nbsp;</label>
-			</div>
-			<div style={field}>
-				<label style={label}>Nova Senha</label>
-				<input style={input} disabled={true} value='' />
-				<label>&nbsp;</label>
-			</div>
+			<EditableData
+				name='Sobrenome'
+				value={'Barbosa'}
+				onChange={updateFname}
+				validateInput={validateFname}
+				submit={saveFname}
+				setError={setErrorFname}
+				error={errorFname}
+			/>
+			<EditableData
+				name='RG'
+				value={''}
+				onChange={updateFname}
+				validateInput={validateFname}
+				submit={saveFname}
+				setError={setErrorFname}
+				error={errorFname}
+				warning='preencha para liberar pagamentos'
+				placeholder='11.22.33.44-55'
+			/>
+			<EditableData
+				name='CPF'
+				value={''}
+				onChange={updateFname}
+				validateInput={validateFname}
+				submit={saveFname}
+				setError={setErrorFname}
+				error={errorFname}
+				warning='preencha para liberar pagamentos'
+				placeholder='111.222.333-44'
+			/>
+			<EditableData
+				name='Email'
+				value={'vitorbarbosa19@gmail.com'}
+				onChange={updateFname}
+				validateInput={validateFname}
+				submit={saveFname}
+				setError={setErrorFname}
+				error={errorFname}
+				isValidated={true}
+				editable={false}
+			/>
+			<EditableData
+				name='Whatsapp'
+				value={'+55 (11) 95177-1321'}
+				onChange={updateFname}
+				validateInput={validateFname}
+				submit={saveFname}
+				setError={setErrorFname}
+				error={errorFname}
+				editable={false}
+			/>
 		</div>
 	)
 }
