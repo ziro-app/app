8db5024a74a1b64f8c30392f429a26076c9cce96
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-10 21:31:50 -0700

    add new component from ziro library

--- src/App/Menu/index.js
+++ src/App/Menu/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Drawer from '@bit/vitorbarbosa19.ziro.drawer'
-import { Header } from './Header/index'
+import HeaderWithMenu from  '@bit/vitorbarbosa19.ziro.header-with-menu'
 import { Panel } from './Panel/index'
 import { container } from './styles'
 
@@ -9,7 +9,7 @@ export const Menu = ({ title, children }) => {
 	const [isOpen, setIsOpen] = useState(false)
 	return (
 		<div style={container}>
-			<Header title={title} setIsOpen={setIsOpen} />
+			<HeaderWithMenu title={title} setIsOpen={setIsOpen} />
 			<Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
 				<Panel isOpen={isOpen} setIsOpen={setIsOpen} />
 			</Drawer>
--- src/App/Menu/styles.js
+++ src/App/Menu/styles.js
@@ -1,10 +1,7 @@
 export const
 
 container = {
-	position: 'relative',
 	height: '100vh',
 	boxSizing: 'border-box',
-	overflow: 'scroll',
-	margin: '0 auto',
 	padding: '20px 10% 0'
 }
