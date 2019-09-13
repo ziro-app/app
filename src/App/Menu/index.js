b80cf2aa82324d5ebd4a58981b070a1c3292b600
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-12 19:11:28 -0700

    update components from library

--- src/App/Menu/index.js
+++ src/App/Menu/index.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Drawer from '@bit/vitorbarbosa19.ziro.drawer'
 import HeaderWithMenu from  '@bit/vitorbarbosa19.ziro.header-with-menu'
-import { Panel } from './Panel/index'
+import DrawerPanel from '@bit/vitorbarbosa19.ziro.drawer-panel'
+import Icon from '@bit/vitorbarbosa19.ziro.icon'
 import { container } from './styles'
 
 export const Menu = ({ title, children }) => {
@@ -11,7 +12,32 @@ export const Menu = ({ title, children }) => {
 		<div style={container}>
 			<HeaderWithMenu title={title} setIsOpen={setIsOpen} />
 			<Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
-				<Panel isOpen={isOpen} setIsOpen={setIsOpen} />
+				<DrawerPanel
+					username='Vitor Barbosa'
+					usercnpj='28.026.371/0001-61'
+					options={[
+						{
+							path: '#',
+							icon: <Icon type='truck' size={13} strokeWidth={3} />,
+							text: 'Atendimentos'
+						},
+						{
+							path: '#',
+							icon: <Icon type='card' size={13} strokeWidth={3} />,
+							text: 'Pagamentos'
+						},
+						{
+							path: '#',
+							icon: <Icon type='user' size={13} strokeWidth={3} />,
+							text: 'Meus Dados'
+						},
+						{
+							path: '#',
+							icon: <Icon type='logout' size={13} strokeWidth={3} />,
+							text: 'Sair'
+						},
+					]}
+				/>
 			</Drawer>
 			{children}
 		</div>
