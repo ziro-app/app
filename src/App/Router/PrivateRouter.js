37219ae6b0ec821856b83357973d3305978664c8
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-13 18:21:45 -0700

    minor fix

--- src/App/Router/PrivateRouter.js
+++ src/App/Router/PrivateRouter.js
@@ -3,11 +3,11 @@ import { Switch, Route, Redirect } from 'wouter'
 import { MyData } from '../MyData/index'
 import { NotFound } from '../NotFound/index'
 
-export const PrivateRouter = ({ signOut }) =>
+export const PrivateRouter = () =>
 	<Switch>
 		<Route path='/'><Redirect to='/meus-dados/fisica' /></Route>
 		<Route path='/home/login'><Redirect to='/meus-dados/fisica' /></Route>
 		<Route path='/cadastrar/email'><Redirect to='/meus-dados/fisica' /></Route>
-		<Route path='/meus-dados/:any'><MyData signOut={signOut} /></Route>
+		<Route path='/meus-dados/:any'><MyData /></Route>
 		<Route path='/:any*'><NotFound fallback='/meus-dados/fisica' /></Route>
 	</Switch>
--- src/App/Router/index.js
+++ src/App/Router/index.js
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types'
 import { PrivateRouter } from './PrivateRouter'
 import { PublicRouter } from './PublicRouter'
 
-export const Router = ({ userLoggedIn, signOut }) =>
-	userLoggedIn ? <PrivateRouter signOut={signOut} /> : <PublicRouter />
+export const Router = ({ userLoggedIn }) =>
+	userLoggedIn ? <PrivateRouter /> : <PublicRouter />
 
 Router.propTypes = {
-	userLoggedIn: PropTypes.bool,
-	signOut: PropTypes.func
+	userLoggedIn: PropTypes.bool
 }
