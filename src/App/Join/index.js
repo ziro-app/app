d5deb60c3fa3222fca194eba3273ad0d765fbf08
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-15 09:26:29 -0700

    add Join component

---
+++ src/App/Join/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CallToAction from '@bit/vitorbarbosa19.ziro.call-to-action'
+
+export const Join = () =>
+	<CallToAction
+		headerText='Tem cadastro? '
+		headerLinkPath='/home'
+		headerLinkText='login'
+		pillText='Faça parte'
+		ctaText='Compre pra revender sem se preocupar com fornecedores, logística ou pagamento'
+		btnPath='/cadastrar/cnpj'
+		btnText='Começar'
+	/>
--- src/App/Router/PublicRouter.js
+++ src/App/Router/PublicRouter.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'wouter'
 import { Home } from '../Home/index'
+import { Join } from '../Join/index'
 import { Register } from '../Register/index'
 
 export const PublicRouter = () =>
 	<Switch>
 		<Route path='/home/:any'><Home /></Route>
+		<Route path='/cadastrar'><Join /></Route>
 		<Route path='/cadastrar/:any'><Register /></Route>
-		<Route path='/cadastrar'><Redirect to='/cadastrar/cnpj' /></Route>
 		<Route path='/:any*'><Redirect to='/home/login' /></Route>
 	</Switch>
