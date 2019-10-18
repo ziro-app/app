87129ee55578cbf78cc42f8af1d624edd5052080
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-18 16:00:41 -0700

    add isError state and spinner

--- src/App/CardCheckout/index.js
+++ src/App/CardCheckout/index.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { db } from '../../Firebase/index'
 import Header from '@bit/vitorbarbosa19.ziro.header'
 import Checkout from '@bit/vitorbarbosa19.ziro.checkout'
+import Spinner from '@bit/vitorbarbosa19.ziro.spinner'
 import { containerWithPadding } from '../../Theme/styleVariables'
 
 export const CardCheckout = () => {
 	const [isLoading, setIsLoading] = useState(true)
+	const [isError, setIsError] = useState(false)
 	const [charge, setCharge] = useState('')
 	const [maxInstallments, setMaxInstallments] = useState('')
 	const [seller, setSeller] = useState('')
@@ -21,15 +23,23 @@ export const CardCheckout = () => {
 					setMaxInstallments(maxInstallments)
 					setSeller(seller)
 					setIsLoading(false)
-				}
-			}
+				} else setIsError(true)
+			} else setIsError(true)
 		}
 		fetchPaymentFromDb()
 	}, [])
 	return (
 		<div style={containerWithPadding}>
-			<Header type='title-only' title='Pagamento' />
-			<Checkout charge={charge} maxInstallments={maxInstallments} seller={seller} />
+			{isError
+				? <div>Error</div>
+				: <>
+					<Header type='title-only' title='Pagamento' />
+					{isLoading
+						? <div style={{ display: 'grid' }}><Spinner size={'6rem'} /></div>
+						: <Checkout charge={charge} maxInstallments={maxInstallments} seller={seller} />
+					}
+				  </>
+			}
 		</div>
 	)
 }
