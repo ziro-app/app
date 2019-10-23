ef29de98af1cd79d5c4f5c4df0ba2541838adecf
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-22 18:23:20 -0700

    add loading and error loading states

--- src/App/MyPayments/index.js
+++ src/App/MyPayments/index.js
@@ -2,32 +2,37 @@ import React, { useState, useEffect, useContext } from 'react'
 import { userContext } from '../appContext'
 import { db } from '../../Firebase/index'
 import currencyFormat from '@ziro/currency-format'
+import Spinner from '@bit/vitorbarbosa19.ziro.spinner'
+import ErrorLoading from '@bit/vitorbarbosa19.ziro.error-loading'
 import Timeline from '@bit/vitorbarbosa19.ziro.timeline'
-import {} from './styles'
+import { spinner } from './styles'
 
 export const MyPayments = () => {
+	const [isLoading, setIsLoading] = useState(true)
+	const [errorLoading, setErrorLoading] = useState(false)
 	const [payments, setPayments] = useState([])
 	const { docId: buyer } = useContext(userContext)
-	useEffect(() => {
-		return db.collection('credit-card-payments').where('buyer','==',buyer).onSnapshot(
-			snapshot => {
-				if (!snapshot.empty) {
-					let payments = []
-					snapshot.forEach(doc => {
-						const { seller, charge, status, date } = doc.data()
-						const chargeFormatted = currencyFormat(charge)
-						const dateFormatted = new Date(date.seconds * 1000).toLocaleDateString('pt-br', { day: '2-digit', month: 'short' }).replace(' de ','/')
-						payments.push({ seller, charge: chargeFormatted, status, date: dateFormatted  })
-					})
-					setPayments(payments)
-				}
-			},
-			error => {
-				console.log(error)
+	useEffect(() => db.collection('credit-card-payments').where('buyer','==',buyer).onSnapshot(
+		snapshot => {
+			if (!snapshot.empty) {
+				let payments = []
+				snapshot.forEach(doc => {
+					const { seller, charge, status, date } = doc.data()
+					const chargeFormatted = currencyFormat(charge)
+					const dateFormatted = new Date(date.seconds * 1000).toLocaleDateString('pt-br', { day: '2-digit', month: 'short' }).replace(' de ','/')
+					payments.push({ seller, charge: chargeFormatted, status, date: dateFormatted  })
+				})
+				setPayments(payments)
 			}
-		)
-	}, [buyer])
-	return (
-		<Timeline transactions={payments} />
-	)
+			if (isLoading) setIsLoading(false)
+		},
+		error => {
+			console.log(error)
+			setErrorLoading(true)
+			if (isLoading) setIsLoading(false)
+		}
+	), [buyer])
+	if (isLoading) return <div style={spinner}><Spinner size='5.5rem' /></div>
+	if (errorLoading) return <ErrorLoading />
+	return <Timeline transactions={payments} />
 }
