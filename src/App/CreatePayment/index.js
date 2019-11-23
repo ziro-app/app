5979b3f7b35c1855909fa5dd2a893e9612f4424d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-11-23 08:39:07 -0800

    add isLoading state

--- src/App/CreatePayment/index.js
+++ src/App/CreatePayment/index.js
@@ -4,6 +4,7 @@ import currencyFormat from '@ziro/currency-format'
 import maskInput from '@ziro/mask-input'
 import { sendToBackend } from './sendToBackend'
 import capitalize from '@ziro/capitalize'
+import ErrorLoading from '@bit/vitorbarbosa19.ziro.error-loading'
 import Header from '@bit/vitorbarbosa19.ziro.header'
 import Form from '@bit/vitorbarbosa19.ziro.form'
 import FormInput from '@bit/vitorbarbosa19.ziro.form-input'
@@ -76,52 +77,57 @@ export const CreatePayment = () => {
 	}, [])
 	return (
 		<div style={containerWithPadding}>
-			<Header type='title-only' title='Nova cobrança' />
-			<Form
-				buttonName='Criar cobrança'
-				validations={validations}
-				sendToBackend={sendToBackend(state)}
-				inputs={[
-					<FormInput
-						name='seller'
-						label='Vendedor'
-						input={
-							<Dropdown
-								value={seller}
-								onChange={({ target: { value } }) => setSeller(value)}
-								list={sellers}
-								placeholder='Nome do fabricante'
-								onChangeKeyboard={element => element ? setSeller(element.value) : null }
+			{isError
+				? <ErrorLoading message='Recarregue a página ou contate suporte' />
+				: <>
+					<Header type='title-only' title='Nova cobrança' />
+					<Form
+						buttonName='Criar cobrança'
+						validations={validations}
+						sendToBackend={sendToBackend(state)}
+						inputs={[
+							<FormInput
+								name='seller'
+								label='Vendedor'
+								input={
+									<Dropdown
+										value={seller}
+										onChange={({ target: { value } }) => setSeller(value)}
+										list={sellers}
+										placeholder='Nome do fabricante'
+										onChangeKeyboard={element => element ? setSeller(element.value) : null }
+									/>
+								}
+							/>,
+							<FormInput
+								name='charge'
+								label='Valor a cobrar'
+								input={
+									<InputText
+										value={currencyFormat(charge)}
+										onChange={({ target: { value } }) => {
+											const toInteger = parseInt(value.replace(/[R$\.,]/g,''),10)
+											return setCharge(maskInput(toInteger,'#######',true))
+										}}
+										placeholder='R$1.299,99'
+									/>
+								}
+							/>,
+							<FormInput
+								name='maxInstallments'
+								label='Parcelamento máximo'
+								input={
+									<InputText
+										value={maxInstallments}
+										onChange={({ target: { value } }) => setMaxInstallments(maskInput(value, '#', true))}
+										placeholder='6'
+									/>
+								}
 							/>
-						}
-					/>,
-					<FormInput
-						name='charge'
-						label='Valor a cobrar'
-						input={
-							<InputText
-								value={currencyFormat(charge)}
-								onChange={({ target: { value } }) => {
-									const toInteger = parseInt(value.replace(/[R$\.,]/g,''),10)
-									return setCharge(maskInput(toInteger,'#######',true))
-								}}
-								placeholder='R$1.299,99'
-							/>
-						}
-					/>,
-					<FormInput
-						name='maxInstallments'
-						label='Parcelamento máximo'
-						input={
-							<InputText
-								value={maxInstallments}
-								onChange={({ target: { value } }) => setMaxInstallments(maskInput(value, '#', true))}
-								placeholder='6'
-							/>
-						}
+						]}
 					/>
-				]}
-			/>
+				  </>
+			}
 		</div>
 	)
 }
